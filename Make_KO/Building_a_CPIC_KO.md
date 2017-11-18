9cbe292d64d25bf95c4bc75883038dae63598a34
Koki Sasagawa <koki@253-3.synergydsl.com>
2017-11-17 22:21:48 -0500

    Finished CPIC Wizard

--- Make_KO/Building_a_CPIC_KO.md
+++ Make_KO/Building_a_CPIC_KO.md
@@ -1,66 +1,30 @@
 ## Building a CPIC Knowledge Object
-*last updated 11/2/2017*
+*last updated 11/17/2017*
 
 **Important Notice:**
 The following instructions are designed primarily for Mac users. Windows users may need to have bash installed and configured before following these steps. 
 
 The following link will pull up instructions on installing [bash](https://www.howtogeek.com/249966/how-to-install-and-use-the-linux-bash-shell-on-windows-10/)
 
-Once bash is installed, make sure pip installer has been installed and updated to be the latest version. We will need both the pip installer for python2 and pip3 installer for python3. (This step will likely be removed in upcoming editions)
+Once bash is installed, make sure pip3 installer has been installed and updated to be the latest version.
 
-Once pip is up and running, use the following command to install the xlrd module: `pip3 install xlrd`
+Once pip3 is up and running, use the following command to install the xlrd module: `pip3 install xlrd`. This step is required to read in our excel file. 
 
-Now we are ready to create our first CPIC KO. 
+We are now ready to create our first CPIC KO using the CPIC Wizard.
 
-### Gathering KO parts
+### Instructions:  
+1. Go to kgrids [github](https://github.com/kgrid/CPIC-objects/tree/master/Make_KO) and download the file `CPIC_Wizard.py` to your local file.  
+2. Retrieve and download a official diplotype to phenotype excel table from the CPIC website. Double check that it is saved in the following format: `<gene>_Diplotype_Phenotype_Table.xlsx`. Be sure to save this file in the same directory as `CPIC_Wizard.py`.
+3. Open the excel file and check that it is formatted correctly. Sometimes special characters remain, such as `Metabolizerc` instead of `Metabolizer`. Remove these by manually performing a find and replace. **Instructions on performing find-and-replace on Excel can be found below**
+4. Open the `CPIC_Wizard.py` file and find the global variable called `KOFILE` under KO contents. Change the text `name` to the name of your computers home directory.
+5. If it does not already exist, be sure to create the directory `activator` in your home directory. Once the `activator` directory is created, open it to create another directory inside called `shelf`. This is where the complete KO will be stored. 
+6. Run the following in the terminal command line: `python3 CPIC_Wizard.py <Excel table of interest>`
+7. If successful, the newly made KO should now reside in your local actiators shelf. 
 
-Go to kgrids [github](https://github.com/kgrid/CPIC-objects/tree/master/Make_KO) and download the following files onto your local computer:
- 
-1. build.py
-2. excel_to_dictionary.py
-3. input.xml
-4. output.xml
-5. kog.json
-6. metadata.json
-7. skeleton_geno_pheno-base.py
-8. skeleton_pheno-recom-base.py (**Disregard this file for now. Will be acessible in the future**)
-
-Be sure to check that all of the files are in the same directory. 
-
-### Converting a CPIC excel table into a python dictionary
-1. Retrieve and download a official diplotype to phenotype excel table from the CPIC website.
-2. Save it to local directory with all the other files.  
-3. Run the `excel_to_dictionary.py` python file in the bash command line and passing the excel file of interest as the argument. The command should look like the following: `python3 excel_to_dictionary.py <excel_file_of_interest>`.
-5. Open the local directory and the excel file should now be saved as a textfile which has been formatted into a dictionary.
-6. Make sure to clean up the file to ensure consistency. Sometimes special characters remain, such as `Metabolizerc` instead of `Metabolizer`. Remove these by opening the text editor and manually performing a find and replace in the text editor.
-
-### Creating a CPIC payload
-1. Open the `skeleton_geno_pheno-base.py`.
-2. Scroll down to where it says "COPY AND PASTE DICTIONARY HERE".
-3. Below, you will find a dictionary variable named `geno_pheno`. 
-4. Open the textfile created earlier, and copy the contents and paste it into the right of the `=` sign. 
-5. Rename the `skeleton_geno_pheno-base.py` file by replacing `skeleton` with the name of the gene of interest. 
-6. Save and close the file. You have now successfully created the payload. 
-
-### Configuring the kog.json file
-1. Open the `kog.json`.  
-2. Go to where it says `"payloadfile":"some_python_file.py"`.
-3. Change `some_python_file.py` with the name of the python payload you created earlier. It should look something like this: `"payloadfile":"UTG1A1_geno_pheno-base.py"`
-4. Go to where it says `"KOFile":"somepath"`.
-5. Change `somepath` with the direct path to the activators shelf on your local computer, then add the name of the KO you are creating at the end. It should look something like this: `"KOFile":"/Users/sam/activator/shelf/UTG1A1-object"`
-6. Save and close the file. 
-
-### Creating the metadata.json file
-1. Open the `metadata.json`.
-2. Go to where it says `"title":"some_title"`
-3. Change `some_title` to the desired KO name. It should look something like this: `"title":"UTG1A1-object"`
-4. Go to where it says `"functionName":"some_function"`
-5. change `some_function` to the main function name called in the payload.
-6. Save and close the file. 
-
-### Building the KO
-1. Open terminal and run `build.py` with kog.json as its argument. The command should look like this: `python build.py kog.json`. It is important to use `python` instead of `python3` since build.py is written in python2.
-3. If successful, the newly made KO should now reside in your local actiators shelf. 
-
-### Test the KO is functioning by accesssing it via Postman. 
-"content soon to come"
+### Find-and-Replace on Excel:
+1. Go to the excel menu bar and click `edit`. 
+2. Go to where it says `find` which will open additional choices.
+3. Click `replace`. This should open a new window. 
+4. Under `Find what:` enter in the text we want to replace. 
+5. Under `Replace with:` enter in the new text of interest. 
+6. Click `Replace All` to complete the find and replace. 
--- Make_KO/CPIC_Wizard.py
+++ Make_KO/CPIC_Wizard.py
@@ -1,5 +1,5 @@
 # Utility Wizard: CPIC KO Generator 
-# Last Updated: 11/12/2017
+# Last Updated: 11/17/2017
 # Koki Sasagawa
 # KGrid
 # The following code will accept a excel table file and generate a CPIC KO
@@ -8,7 +8,7 @@
 import xlrd
 import json
 import sys
-
+# import unittest # Comment out for actual use. 
 
 # Initialize global variables to default settings:
 # 1. SPSHEET will be the excel spreadsheet of interest.
@@ -43,44 +43,51 @@ EXTENSIONS = ['.xlsx']
 # KO contents: 
 PAYLOAD = "def alleleFormat(geno):\n\t\"\"\" \n\tThe following code will swap the order of alleles to always have the smaller number as allele1 and the larger number as allele2. It will also add '*' to change to star format if previous input was not. \n\t\"\"\"\n\tif geno['diplotype']:\n\t\tdiplotype = geno['diplotype']\n\t\tlist_diplotype = diplotype.split('/')\n\t\t# If in starformat, remove. Otherwise it will be left unchanged. \n\t\tallele1 = int(list_diplotype[0].replace('*',''))\n\t\tallele2 = int(list_diplotype[1].replace('*',''))\n\t\t# If allele1 is greater than allele2, swap the order.\n\t\tif allele1 > allele2:\n\t\t\tgeno['diplotype'] = '*' + str(allele2) + '/' + '*' + str(allele1) \n\n\telif geno['allele1'] and geno['allele2']:\n\t\t# If allele is in starformat, remove. Otherwise it will be left unchanged. \n\t\tallele1 = int(geno['allele1'].replace('*',''))\n\t\tallele2 = int(geno['allele2'].replace('*',''))\n\t\t# If allele1 is greater than allele2, swap the order.\n\t\tif allele1 > allele2:\n\t\t\tgeno['allele1'] = '*' + str(allele2)\n\t\t\tgeno['allele2'] = '*' + str(allele1)\n\ndef execute(geno):\n\t\"\"\" The following function will return the phenotype corresponding to the user specified genotype \"\"\"\n\n\t# Dictionary containing Genotype to Phenotype Information  \n\tgeno_pheno = {}\n\n\t# Arranging the order so the lower numerical allele is on the left. Also add a star '*' if not in star allele format. \n\talleleFormat(geno)\n\n\t# Get appropriate phenotype corresponding to gene\n\tif geno['diplotype']:\n\t\tif geno['diplotype'] in geno_pheno:\n\t\t\t# Assign appropriate phenotype pair value for the corresponding key(diplotype) from dictonary geno_pheno\n\t\t\tgeno['phenotype'] = geno_pheno[geno['diplotype']]\n\t\telse:\n\t\t \treturn(\"Incorrect/invalid input for diplotype\")\n\n\telif geno['allele1'] and geno['allele2']:\n\t\t# Convert allele to diplotype format\n\t\tcombine_allele = geno['allele1'] + '/' + geno['allele2']\n\t\tif combine_allele in geno_pheno:\n\t\t\tgeno['phenotype'] = geno_pheno[combine_allele]\n\t\telse:\n\t\t \treturn(\"Incorrect/invalid input for allele\")\n\n\treturn str(geno[\"phenotype\"])"
 INPUTXML = "<rdf:RDF xmlns:ot=\"http://uofm.org/objectteller/#\"\n         xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\">\n    <rdf:Description rdf:about=\"http://uofm.org/objectteller/inputMessage\">\n        <ot:noofparams>4</ot:noofparams>\n        <ot:params>\n            <rdf:Seq>\n                <rdf:li>diplotype</rdf:li>\n        <rdf:li>allele1</rdf:li>\n         <rdf:li>allele2</rdf:li>\n   <rdf:li>phenotype</rdf:li>\n            </rdf:Seq>\n        </ot:params>\n    </rdf:Description>\n    <rdf:Description rdf:about=\"http://uofm.org/objectteller/age/\">\n        <ot:datatype>STRING</ot:datatype>\n    </rdf:Description>\n</rdf:RDF>\n"
-OUTPUTXML = "outputMessage":"\n<rdf:RDF xmlns:ot=\"http://uofm.org/objectteller/\"\n  xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\">\n  <rdf:Description rdf:about=\"http://uofm.org/objectteller/outputMessage\">\n    <ot:returntype>STRING</ot:returntype>\n  </rdf:Description>\n</rdf:RDF>\n"
+OUTPUTXML = "<rdf:RDF xmlns:ot=\"http://uofm.org/objectteller/\"\n  xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\">\n  <rdf:Description rdf:about=\"http://uofm.org/objectteller/outputMessage\">\n    <ot:returntype>STRING</ot:returntype>\n  </rdf:Description>\n</rdf:RDF>\n"
+KOFILE = "/Users/koki/activator/shelf/{}-Object"
 METADATA = {
 	"metadata": {
 		"title": ""
 		}, 
+	"inputMessage": "",
+	"outputMessage": "",
 	"payload": {
 		"content":"",
     	"engineType": "PYTHON",
-    	"functionName": "some_function"
+    	"functionName": "execute"
     	}
 	}
 
-# {
-#   "metadata":{
-#     "title":""
-#   },
-#   "payload":{
-#     "content":"",
-#     "engineType":"PYTHON",
-#     "functionName":"some_function"
-#   }
-# }
 
-
-
-# Excel file evaluator
 def excel_file_filter(filename):
 	""" Returns True if file is excel table. """
-	return any(filename.endswith(e) for e in EXTENSIONS)
+	file_check = any(filename.endswith(e) for e in EXTENSIONS)
+	
+	if file_check == False:
+		print("INVALID: File type is not valid. Please check that file extension is {}".format(EXTENSIONS))
+		return file_check
+	else:
+		return file_check
 
 
-# Input evaluator 
 def input_evaluator(sheetIndex, startRow, endRow):
 	""" Checks that excel table specifications are invalid. """
-	if ((sheetIndex < 0) or (startRow < 0) or (endRow < 0) or (endRow < startRow)):
+	if ((sheetIndex < 0) or (startRow < 0) or (endRow < 0)):
+		print("INVALID: Numerical specifications must be positive numbers.")
 		return False
+	elif endRow > 0:
+		if endRow < startRow:
+			print("INVALID: Ending row must be larger than starting row.")
+			return False
 	else:
 		return True
+
+
+def file_name(spSheet):
+	""" Reformat file name to be shorter """
+	parts = spSheet.split('_')
+	short_name = parts[0]
+	return short_name
 
 
 def excel_to_dictionary(spSheet, sheetIndex, startRow, endRow):
@@ -90,19 +97,16 @@ def excel_to_dictionary(spSheet, sheetIndex, startRow, endRow):
 	try:
 		excel_file = xlrd.open_workbook(spSheet)
 	except:
-		return ('Invalid Request: File does not exist or path-to-file is incorrect.')
-
-	# Assign the file name. Remove the file type extension. 
-	# for ending in EXTENSIONS:
-	# 	if spSheet.endswith(ending):
-	# 		file_name = spSheet[:-len(ending)]
+		print('INVALID: File does not exist or path-to-file is incorrect.')
+		return False
 
 	# Specify the sheet of the excel file
 	try: 
 		table1 = excel_file.sheet_by_index(sheetIndex)
 	except:
-		return ('Invalid Request: Invalid input or sheet does not exist.')
-		
+		print('INVALID: Invalid input or sheet does not exist.')
+		return False
+
 	# Initialize result dictionary 
 	genotype_to_phenotype = {}
 	
@@ -126,6 +130,11 @@ def excel_to_dictionary(spSheet, sheetIndex, startRow, endRow):
 			genotype_to_phenotype[genotype] = phenotype
 
 	# # Generate a text file 
+	# # Assign the file name. Remove the file type extension. 
+	# for ending in EXTENSIONS:
+	# 	if spSheet.endswith(ending):
+	# 		file_name = spSheet[:-len(ending)]
+	
 	# with open('Output_{}_file'.format(file_name), 'w') as file:
 	# 	file.write(json.dumps(genotype_to_phenotype))
 
@@ -137,89 +146,79 @@ def excel_to_dictionary(spSheet, sheetIndex, startRow, endRow):
 
 
 # KO generator 
-def build(metadataFile,inputFile,outputFile,payloadFile,KOFile):
-	# open & read files
-	# try:
-	# 	in_file_metadata = open(metadataFile).read()
-	# 	in_file_payload = open(payloadFile).read()
-	# 	in_file_input = open(inputFile).read()
-	# 	in_file_output = open(outputFile).read()
-	# except IOError:
-	# 	print("No such file or directory")
-	# 	return None
+def build(metadata, inputxml, outputxml, payload, KOFile, title):
 
 	# loading in files as json
-	try:
-		backbone = json.loads(in_file_metadata)
-		backbone["inputMessage"] = in_file_input
-		backbone["outputMessage"] = in_file_output
-		backbone["payload"]["content"] = in_file_payload
-
-	except ValueError as e:
-		print('invalid json: %s' % e)
-		return None
+	backbone = metadata
+	backbone["metadata"]["title"] = 'CPIC {} Genotype-to-Phenotype'.format(title)
+	backbone["inputMessage"] = inputxml
+	backbone["outputMessage"] = outputxml
+	backbone["payload"]["content"] = payload
 
 	# dump result into new files
 	with open(KOFile, 'w') as outfile:
 	    output = json.dump(backbone, outfile)
-	return "*****DONE*****"
-
-# Ask for inputs
-# def commandInput():
-# 	metadataFile = raw_input("Enter Metadata File: ")
-# 	inputFile = raw_input("Enter Input File: ")
-# 	outputFile = raw_input("Enter Output File: ")
-# 	payloadFile = raw_input("Enter Payload File: ")
-# 	KOFile = raw_input("Knowledge Object Output File: ")
-
-# 	output = build(metadataFile,inputFile,outputFile,payloadFile,KOFile)
-
-# 	return output
-
-# def buildFromSpecification(specFile):
-#     # load contents on json file
-#     inFile = open(specFile).read()
-#     content = json.loads(inFile)
-
-#     metadataFile = content["metadataFile"]
-#     inputFile = content["inputFile"]
-#     outputFile = content["outputFile"]
-#     payloadFile = content["payloadFile"]
-#     KOFile = content["KOFile"]
-
-#     output = build(metadataFile,inputFile,outputFile,payloadFile,KOFile)
-
-#     return output
-
-# print(buildFromSpecification(sys.argv[1]))
-
 
 
 def main(spSheet, sheetIndex, startRow, endRow):
+	# Assign local variables from global variables 
+	metadata = METADATA
+	inputxml = INPUTXML
+	outputxml = OUTPUTXML
+	payloadfile = PAYLOAD
+	kofile = KOFILE
+
+	if spSheet == '':
+		print("Please enter a file.")
+		return
 
 	if excel_file_filter(spSheet):
 		if input_evaluator(sheetIndex, startRow, endRow):
 			geno_pheno_dict = excel_to_dictionary(spSheet, sheetIndex, startRow, endRow)
-		else: 
-			print("INVALID: Numerical specifications are not valid. please try again.")
-	else: 
-		print("INVALID: Wrong file type")
-	
-	# If isinstance(geno_pheno_dict, str):
-
-	payloadFile = PAYLOAD.format(geno_pheno_dict)
-
-	print(payloadFile)
+			# Make the payload
+			payloadfile = payloadfile.format(geno_pheno_dict)
+			# Create the file name
+			name = file_name(spSheet)
+			# Assign file name to KOFILE
+			kofile = kofile.format(name)
+			# Create the KO
+			build(metadata, inputxml, outputxml, payloadfile, kofile, name)
+			print("*****DONE*****")
 
 
 #################### EXECUTE ####################
 main(SPSHEET, SHEETINDEX, STARTROW, ENDROW)
-excel_to_dictionary(SPSHEET, SHEETINDEX, STARTROW, ENDROW)
-print(PAYLOAD)
-print(INPUTXML)
-print(OUTPUTXML)
-print(METADATA)
-
-#################### EXECUTE ####################
 
 #################### TESTING ####################
+########### COMMENT OUT WHEN IN USE #############
+
+# class excel_file_filter_test(unittest.TestCase):
+# 	def test1(self):
+# 		self.assertEqual(excel_file_filter('UGT1A1_Diplotype_Phenotype_Table.xlsx'), True)
+# 	def test2(self):
+# 		self.assertEqual(excel_file_filter('UGT1A1_Diplotype_Phenotype_Table.xls'), False)
+
+# class input_evaluator_test(unittest.TestCase):
+# 	def test1(self):
+# 		self.assertEqual(input_evaluator(0, 1, 0), True, 'Default numbers should return True.')
+# 	def test2(self):
+# 		self.assertEqual(input_evaluator(0, -1, 0), False, 'Negative start row number should return False.')
+# 	def test3(self):
+# 		self.assertEqual(input_evaluator(-1, 0, 0), False, 'Negative sheet index should return False.')
+# 	def test4(self):
+# 		self.assertEqual(input_evaluator(0, 0, -1), False, 'Negative end row number should return False.')
+# 	def test5(self):
+# 		self.assertEqual(input_evaluator(0, 16, 4), False, 'startRow less than endRow should return False.')
+
+# class file_name_test(unittest.TestCase):
+# 	def test1(self):
+# 		self.assertEqual(file_name('UGT1A1_Diplotype_Phenotype_Table.xlsx'), 'UGT1A1', 'Incorrect text parsing. Check code.')
+# 	def test2(self):
+# 		self.assertEqual(file_name('CYP2C9_Diplotype_Phenotype_Table.xlsx'), 'CYP2C9', 'Incorrect text parsing. Check code.')
+# 	def test3(self):
+# 		self.assertEqual(file_name('CYP2C19_Diplotype_Phenotype_Table.xlsx'), 'CYP2C19', 'Incorrect text parsing. Check code.')
+
+# if __name__ == '__main__':
+# 	unittest.main(verbosity=2)
+
+
