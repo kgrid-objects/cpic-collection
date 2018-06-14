6934e39784df50228f2eef3525796fe1d161d1d1
Jack <jackall@umich.edu>
2018-06-14 18:32:38 -0400

    create mme_conversion, add file outline and necessary excel input

---
+++ mme_conversion/README.md
@@ -0,0 +1,15 @@
+README.md
+
+
+# Morphine Milligram Equivalent Calculator
+[RxNAV](https://rxnav.nlm.nih.gov/)
+[Ohio MED Calculator](https://www.ohiopmp.gov/MED_Calculator.aspx)
+
+
+## Data Preparation (TestOpiodPrescriptions.xlsx)
+
+- removed one-time dosing (not necessary for dealing with daily MME overtime
+- removed /min dosing, used when leading up to surgery
+- removed "continuous" dosing, these are drip IVs which only give a range for dosing information
+
+
---
+++ mme_conversion/mme_conversion.js
@@ -0,0 +1,64 @@
+// mme_conversion.js
+// MME Conversion Calculator
+
+// Created by Jack Allan (jackall@umich.edu) on June 14th, 2018
+
+
+
+// ------------------------------------------------------------------------------------------------ //
+// INPUT
+
+	// rules
+
+		/*
+
+			max amount for ranges (ex. "2-4" = "4")
+
+			"bedtime" and "every mornign" = once / day
+
+
+		*/
+
+
+	// error return for insufficient input
+
+
+
+	// parsing
+
+
+
+// ------------------------------------------------------------------------------------------------ //
+// COMPUTATION
+
+
+	/*
+
+	convert to amount daily
+
+
+	adjust units as necessary
+
+
+	multiple by conversion factor
+
+
+	add
+
+
+
+
+
+
+
+
+
+
+// ------------------------------------------------------------------------------------------------ //
+// OUTPUT
+
+	// list individual opiods, mg of that opiod, and mg equivalent for morphine... followed by total morphine amount
+
+
+
+	// response body including all information from RxNav APIs
