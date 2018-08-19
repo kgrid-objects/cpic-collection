8fc936d6d8246117ea5f612fd0110fbef7ce7808
Jay Deshpande <djaylaxm@med.umich.edu>
2018-08-19 15:25:18 -0400

    updared PayloadScript.js

--- scripts/g2pPayloadScript.js
+++ scripts/g2pPayloadScript.js
@@ -4,8 +4,44 @@ fs.createFile(file, function(err) {
   console.log(err); //null
   //file has now been created, including the directory it is to be placed in
 })
-fs.writeFile("payload.js", "\n"
+fs.writeFile("payload.js", "\n" + ""
 + "function getphenotype (inputs) {" + "\n"
++"var diplotype = ''" +"\n"
++"var output = {'enzym': 'CYP2D6', 'phenotype': ''}" + "\n"
++"try {" + "\n"
+  + "var enzym = inputs.enzym || ''" + "\n"
+  + "var a1 = inputs['allele1'] || ''" + "\n"
+  + "var a2 = inputs['allele2'] || ''" + "\n"
+  + "if ((enzym != '') && (a1 != '') && (a2 != '')) {" + "\n"
+  + " if (enzym == output.enzym) {" + "\n"
+  +    "diplotype = a1 + '-' + a2" + "\n"
+  +    "var index = list[diplotype]" + "\n"
+  +    "if (index == null) {" + "\n"
+  +      "diplotype = a2 + '-' + a1" + "\n"
+  +      "index = list[diplotype]" + "\n"
+  +   "} "+ "\n"
+  + "if (index == null) {" + "\n"
+  +  "output.phenotype = 'Unknown'" + "\n"
+  +"} else {" + "\n"
+  +  "if (index == 0) {" + "\n"
+  +    "output.phenotype = dict[0]" + "\n"
+  + " } else {" + "\n"
+  +    "output.phenotype = dict[index] + ' metabolizer'"
+  + " }" + "\n"
+  +"}" + "\n"
+  +"return output" + "\n"
+  +"} else {" + "\n"
+  +"return 'Expecting ' + output.enzym + ' as enzym input for this KO.'" + "\n"
+  +"}" + "\n"
++"} else {" + "\n"
++  "return 'Invalid Input'" + "\n"
++"}" + "\n"
++"} catch (error) {" + "\n"
++"return 'Error'" + "\n"
++"}" + "\n"
++"}" + "\n"
++"var dict=['TBD','Likely Poor','Poor','Likely Intermediate','Intermediate','Normal','Rapid','Ultrarapid']" + "\n"
+
 + "var list = {'*3-*3':2,'*3-*3xN':2,'*3-*4':2}" , function(err) {
     if(err) {
         return console.log(err);
@@ -13,12 +49,6 @@ fs.writeFile("payload.js", "\n"
 
     console.log("The file was saved!");
 });
-fs.writeFile("payload.js", "var list = {'*3-*3':2,'*3-*3xN':2,'*3-*4':2}", function(err) {
-    if(err) {
-        return console.log(err);
-    }
 
-    console.log("The file was saved!");
-});
 
 //console.info("bbb");
