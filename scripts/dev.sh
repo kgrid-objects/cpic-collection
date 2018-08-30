965c2760039f7f77fa26896d5014823a42c6ea92
Greg Farris <greg.t.farris@gmail.com>
2018-08-30 17:33:57 -0400

    Added run dev script that will download most current releases activator and start activator using this repo as the shelf shell script now but windows bat to follow

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env sh
 
-java -jar activator/kgrid-activator-0.6.2.jar --kgrid.shelf.cdostore.url=filesystem:file://. $*
+if [ ! -d "dev" ]; then
+  mkdir -p dev
+
+  artifacts=($(curl -s https://api.github.com/repos/kgrid/kgrid-activator/releases/latest | jq -r ".assets[] | .browser_download_url "))
+
+  for url in "${artifacts[@]}"
+
+  do
+     if [[ $url = *.jar ]]
+     then
+        curl -Lo dev/kgrid-activator.jar $url
+     fi
+  done
+
+fi
+
+java -jar dev/kgrid-activator.jar --kgrid.shelf.cdostore.url=filesystem:file:///$PWD
