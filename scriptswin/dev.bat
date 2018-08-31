e35c707f21e20a981e37768770064a8c205cd2af
George Meng <gqmeng@umich.edu>
2018-08-31 12:09:31 -0400

    Modified dev script for windows; Added cpic spreadsheet

--- scriptswin/dev.bat
+++ scriptswin/dev.bat
@@ -1,2 +1,3 @@
-@ECHO OFF
-java -jar activator/kgrid-activator-0.6.6.jar --kgrid.shelf.cdostore.url=filesystem:file:/c:/kgridobjrepo/cpic-objects %*
+
+set str=%cd:\=/%
+java -jar activator/kgrid-activator-0.6.6.jar --kgrid.shelf.cdostore.url=filesystem:file:///%str%
