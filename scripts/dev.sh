7de02d1c7378e666af19b05daa140bf153aa8c5b
Peter Boisvert <pboisver@umich.edu>
2018-08-31 15:14:53 -0400

    `*nix` example using `download-cli` npm package instead of `curl` to improve portability

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -7,15 +7,20 @@ fi
 
 if [ ! -f dev/kgrid-activator*.jar ]; then
 
-  artifacts=($(curl -s https://api.github.com/repos/kgrid/kgrid-activator/releases/latest | jq -r ".assets[] | .browser_download_url "))
+#  artifacts=($(curl https://api.github.com/repos/kgrid/kgrid-activator/releases/latest | jq -r ".assets[] | .browser_download_url "))
+  artifacts=($(download https://api.github.com/repos/kgrid/kgrid-activator/releases/latest >&1 | jq -r ".assets[] | .browser_download_url "))
+
+  echo fetched browser_download_url... ${artifacts}
 
   for url in "${artifacts[@]}"
 
   do
-     if [[ $url = *.jar ]]
+     if [[ ${url} = *.jar ]]
      then
-        echo downloading jar file $url ...
-        curl -Lo dev/kgrid-activator.jar $url
+        echo downloading jar file ${url}
+
+#        curl -Lo dev/kgrid-activator.jar $url
+        download ${url} > dev/kgrid-activator.jar
      fi
   done
 
