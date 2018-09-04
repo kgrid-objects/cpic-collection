3b1837f416aba020a361ffcecd29783014cf7c1d
Greg Farris <greg.t.farris@gmail.com>
2018-09-04 14:33:00 -0400

    Refactor fk49z9gr7p for CPIC Kit story https://www.pivotaltracker.com/story/show/160179808

--- tests/99999-fk49z9gr7p/v0.0.3.test.js
+++ tests/99999-fk49z9gr7p/v0.0.3.test.js
@@ -2,7 +2,8 @@ var assert = require('assert');
 var rewire = require('rewire');
 
 //Get load in the js
-var javascript = rewire('../../99999-fk49z9gr7p/v0.0.3/model/resource/getphenotype');
+var javascript = rewire('../../99999-fk49z9gr7p/v0.0.4/model/resource/getphenotype');
+//var javascript = rewire('../../scripts/payload.js');
 
 //Load in the function
 var getphenotype = javascript.__get__("getphenotype");
@@ -10,13 +11,13 @@ var getphenotype = javascript.__get__("getphenotype");
 var testset = [
   {"input":{"enzyme":"CYP2D6","allele1": "*97",  "allele2": "*98"},"output":"TBD" },
   {"input":{"enzyme":"CYP2D6","allele1": "*1x2",  "allele2": "*41"},"output":"Ultrarapid metabolizer" },
-  {"input":{"enzyme":"CYP2D6","allele1": "*9",  "allele2": "*20"},"output":"TBD" },
-  {"input":{"enzyme":"CYP2D6","allele1": "*2",  "allele2": "*27"},"output":"TBD" },
-  {"input":{"enzyme":"CYP2D6","allele1": "*3",  "allele2": "*5"},"output":"TBD" },
+  {"input":{"enzyme":"CYP2D6","allele1": "*9",  "allele2": "*20"},"output":"Intermediate metabolizer" },
+  {"input":{"enzyme":"CYP2D6","allele1": "*2",  "allele2": "*27"},"output":"Normal metabolizer" },
+  {"input":{"enzyme":"CYP2D6","allele1": "*3",  "allele2": "*5"},"output":"Poor metabolizer" },
   {"input":{"enzyme":"CYP2D6","allele1": "*1002",  "allele2": "*27"},"output":"Unknown" }
 ]
 
-describe('99999-2d6genopheno/v0.0.1', function () {
+describe('99999-2d6genopheno/v0.0.4', function () {
 
   describe('Valid inputs', function(){
 
