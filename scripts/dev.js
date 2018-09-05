77778a1ce6f00ed44478c28c118fdc7bf0f577fe
Greg Farris <greg.t.farris@gmail.com>
2018-09-05 11:14:17 -0400

    Update npm run dev to node script

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,37 +1,55 @@
 const fs = require('fs');
-const dev = require('download');
+const download = require('download');
 const request = require('request');
 const jq = require('node-jq')
 const shell = require('shelljs');
+const fileExists = require('file-exists');
 
 const filter = '.assets[].browser_download_url'
 const jqOptions = { input: 'string', output: 'json'}
 
-var options = {
-    url: 'https://api.github.com/repos/kgrid/kgrid-activator/releases/latest',
-    headers: {
-        'User-Agent': 'request'
-    }
-};
+  var requestOptions = {
+      url: 'https://api.github.com/repos/kgrid/kgrid-activator/releases/latest',
+      headers: {
+          'User-Agent': 'request'
+      }
+  };
 
 
-request(options, function (error, response, body) {
+  request(requestOptions, function (error, response, body) {
     jq.run(filter, body, jqOptions).then((output) => {
-        download_url=output
-        console.log("dowloading " + output)
-        dev(output,".").then(() => {
+
+      let download_url=output;
+
+      fileExists( download_url.substring( (download_url.lastIndexOf('/')+1) ) ).then( exists => {
+        if (exists){
+          console.log("already have activator");
+          executeActivator();
+        } else {
+          console.log(cliSpinners.point);
+          download(output,".").then(() => {
             console.log('files downloaded!');
-            shell.exec("java -jar kgrid-activator*.jar --kgrid.shelf.cdostore.url=filesystem:file:///"+process.cwd(),function(code, stdout, stderr) {
-                console.log('Exit code:', code);
-                console.log('Program output:', stdout);
-                console.log('Program stderr:', stderr);
-            });
-        });
+            executeActivator();
+          });
+        }
+      });
 
     }).catch((err) => {
-        console.error(err)
-    })
-});
+      console.error(err);
+    });
+  });
+
+  function executeActivator() {
+    shell.exec("java -jar kgrid-activator*.jar --kgrid.shelf.cdostore.url=filesystem:file:///"+process.cwd(),function(code, stdout, stderr) {
+      console.log('Exit code:', code);
+      console.log('Program output:', stdout);
+      console.log('Program stderr:', stderr);
+    });
+  }
+
+
+
+
 
 
 
