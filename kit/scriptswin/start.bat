b722fa6b6beb59159f2c9e4301357facdcffed6f
farrisg <farrisg@umich.edu>
2018-09-07 17:20:01 -0400

    Updated windows scripts

--- kit/scriptswin/start.bat
+++ kit/scriptswin/start.bat
@@ -1,6 +1,6 @@
 
 echo "Starting KGrid Library on http://localhost:8081"
-start java -jar library/kgrid-library.jar --server.port=8081 --kgrid.shelf.cdostore.url=filesystem:file://library/shelf
+start "KGrid Library" java -jar library/kgrid-library.jar --server.port=8081 --kgrid.shelf.cdostore.url=filesystem:file://library/shelf
 
 echo "Starting KGrid Activator on http://localhost:8082"
-start java -jar activator/kgrid-activator.jar --server.port=8082 --kgrid.shelf.cdostore.url=filesystem:file://activator/shelf
+start "KGrid Activator" java -jar activator/kgrid-activator.jar --server.port=8082 --kgrid.shelf.cdostore.url=filesystem:file://activator/shelf
