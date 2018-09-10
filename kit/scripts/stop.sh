c811e2a02028fa5fe46a1a21fe132de15c20dde8
George Meng <gqmeng.meng@gmail.com>
2018-09-10 15:21:42 -0400

    Removed /kit after moved to kgrid-demo/cpic-kit

--- kit/scripts/stop.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-pkill -afl http-server
-pkill -afl "8082"
-pkill -afl "8081"
-exit 0
--- kit/scriptswin/stop.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo "Stopping KGrid Activator"
-FOR /F "tokens=5 delims= " %%P IN ('netstat -a -n -o ^| findstr 0:8082') DO IF %%P NEQ 0 ( taskkill /PID %%P /F)
-echo "Stopping KGrid Library"
-FOR /F "tokens=5 delims= " %%P IN ('netstat -a -n -o ^| findstr 0:8081') DO IF %%P NEQ 0 ( taskkill /PID %%P /F)
-echo "Stopping Http Server"
-FOR /F "tokens=5 delims= " %%P IN ('netstat -a -n -o ^| findstr 0:8080') DO IF %%P NEQ 0 ( taskkill /PID %%P /F)
-
-exit/b 0
