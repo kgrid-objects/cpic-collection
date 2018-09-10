b562aa179a7129bc533e10a3004e4cb925822aae
George Meng <gqmeng.meng@gmail.com>
2018-09-10 10:06:30 -0400

    updated start/stop scripts for Windows

--- kit/scriptswin/start.bat
+++ kit/scriptswin/start.bat
@@ -4,3 +4,6 @@ start "KGrid Library" java -jar library/kgrid-library.jar --server.port=8081 --k
 
 echo "Starting KGrid Activator on http://localhost:8082"
 start "KGrid Activator" java -jar activator/kgrid-activator.jar --server.port=8082 --kgrid.shelf.cdostore.url=filesystem:file://activator/shelf
+
+cd web
+node ../node_modules/http-server/bin/http-server -o
--- kit/scriptswin/stop.bat
+++ kit/scriptswin/stop.bat
@@ -3,5 +3,7 @@ echo "Stopping KGrid Activator"
 FOR /F "tokens=5 delims= " %%P IN ('netstat -a -n -o ^| findstr :8082') DO IF %%P NEQ 0 ( taskkill /PID %%P /F)
 echo "Stopping KGrid Library"
 FOR /F "tokens=5 delims= " %%P IN ('netstat -a -n -o ^| findstr :8081') DO IF %%P NEQ 0 ( taskkill /PID %%P /F)
+echo "Stopping Http Server"
+FOR /F "tokens=5 delims= " %%P IN ('netstat -a -n -o ^| findstr :8080') DO IF %%P NEQ 0 ( taskkill /PID %%P /F)
 
 exit/b 0
