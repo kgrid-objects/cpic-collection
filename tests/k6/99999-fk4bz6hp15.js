2f7de560f59994228b68072750117710ebfd3554
Greg Farris <greg.t.farris@gmail.com>
2018-09-21 10:02:28 -0400

    Changed script to us random data for execution of k6 script

--- tests/k6/99999-fk4bz6hp15.js
+++ tests/k6/99999-fk4bz6hp15.js
@@ -1,35 +1,28 @@
 
 import http from "k6/http";
 
-import { check } from "k6";
+import { check, fail } from "k6";
 const params = {headers: {'Content-Type': 'application/json'}};
 let hostname = __ENV.HOSTNAME === undefined ? 'localhost:8080' : __ENV.HOSTNAME
+let testObjects = [
+  {input: {"CYP2C19": {"diplotype": "*1/*20", "phenotype": "Ultrarapid metabolizer"}}, diplotype: "*1/*20", classification:"Strong"},
+  {input: {"CYP2C19": {"diplotype": "*1/*8", "phenotype": "Intermediate metabolizer"}}, diplotype: "*1/*8", classification:"Moderate"},
 
+]
 
 export default function() {
   const url = `http://${hostname}/99999/fk4bz6hp15/v0.0.5/dosingrecommendation`;
 
-  let result = http.post(url, "{\"CYP2C19\": {\"diplotype\": \"*1/*20\", \"phenotype\": \"Ultrarapid metabolizer\"}}", params);
+  var randomtestObject = testObjects[Math.floor(Math.random() * testObjects.length)];
 
-  check(result, {
+  let result = http.post(url,JSON.stringify(randomtestObject.input), params);
+
+
+  check(http.post(url,JSON.stringify(randomtestObject.input), params), {
     'is status 200': (r) => r.status === 200,
-    "diplotype *1/*20 ": (r) => JSON.parse(r.body).result.genes["CYP2C19"].diplotype == "*1/*20",
-    "classification Strong": (r) => JSON.parse(r.body).result.recommendation.classification == "Strong",
-
+    "diplotype *1/*20 ": (r) => JSON.parse(r.body).result.genes["CYP2C19"].diplotype == randomtestObject.diplotype,
+    "classification Strong": (r) => JSON.parse(r.body).result.recommendation.classification == randomtestObject.classification,
   });
 
-   result = http.post(url,
-      "{\"CYP2C19\": {\"diplotype\": \"*1/*8\", \"phenotype\": \"Intermediate metabolizer\"}}", params);
-  check(result, {
-    'is status 200 ': (r) => r.status === 200,
-    "diplotype *1/*8 ": (r) => JSON.parse(r.body).result.genes["CYP2C19"].diplotype == "*1/*8",
-    "classification Moderate": (r) => JSON.parse(r.body).result.recommendation.classification == "Moderate",
-
-  });
 };
 
-
-
-function getFromEnv (variable, defaultValue) {
-  return variable === undefined ? defaultValue : variable
-}
