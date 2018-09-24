8f4ff85d368e241dc478ab923cd8452c7f64203e
George Meng <gqmeng.meng@gmail.com>
2018-09-24 14:41:13 -0400

    Updated geno2pheno lis KO

--- 99999-fk4qj7sz2t/v0.0.4/model/resource/genophenokolist.js
+++ 99999-fk4qj7sz2t/v0.0.4/model/resource/genophenokolist.js
@@ -6,4 +6,4 @@ function genophenokolist(inputs){
   return list;
 }
 
-var kolist={"CFTR": "","CYP2C19":"/99999/fk4mc97w0h/v0.0.4", "CYP2C9" :"","CYP2D6": "/99999/fk49z9gr7p/v0.0.6","CYP3A5": "","CYP4F2": "","DPYD": "","G6PD": "", "HLA-A" : "", "HLA-B" : "",  "IFNL3" : "", "SLCO1B1" : "", "TPMT" : "",  "UGT1A1": "/99999/fk47h1x090/v0.0.4",  "VKORC1" : "",  }
+var kolist={"CFTR": "","CYP2C19":"/99999/fk4mc97w0h/v0.0.4", "CYP2C9" :"","CYP2D6": "/99999/fk49z9gr7p/v0.0.6","CYP3A5": "/99999/fk4md04x9z/v0.0.1","CYP4F2": "","DPYD": "","G6PD": "", "HLA-A" : "", "HLA-B" : "",  "IFNL3" : "", "SLCO1B1" : "/99999/fk47380j09/v0.0.1", "TPMT" : "",  "UGT1A1": "/99999/fk47h1x090/v0.0.4",  "VKORC1" : "",  }
