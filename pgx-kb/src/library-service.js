9507b6f68f50dd1facf4a4102337954daa290088
Farid Seifi <farid.seifi@gmail.com>
2025-06-11 14:57:49 -0400

    library service added to the KB. Renamed the KB folder to pgx-kb

---
+++ pgx-kb/src/library-service.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const { loadKnowledgeFunctions, loadMetadata, loadKnowledgeSet } = require('./knowledgeLoader');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc');
+
+const path = require('path');
+
+let knowledgeSet1 = [];
+let knowledgeSet2 = [];
+
+async function run(input) {
+  const mainMeta = await loadMetadata(path.join(path.join(__dirname, '..'), 'metadata.json'));
+  const lnpwledgeSets = mainMeta['https://kgrid.org/koio#hasKnowledge']
+  knowledgeSet1 = await loadKnowledgeSet(lnpwledgeSets[0]);
+  knowledgeSet2 = await loadKnowledgeSet(lnpwledgeSets[1]);
+
+  knowledgeSet1 = await loadKnowledgeFunctions(knowledgeSet1, 'phenotype');
+  console.log(knowledgeSet1)
+  knowledgeSet2 = await loadKnowledgeFunctions(knowledgeSet2, 'dosingrecommendation');
+
+try {
+    const intermediateResults = await Promise.all(
+      knowledgeSet1.map(ko => ko.function(input['diplotype']))
+    );
+    const mergedResults = intermediateResults.reduce((acc, obj) => {
+      return { ...acc, ...obj };
+    }, {});
+    const finalResults = await Promise.all(
+      knowledgeSet2.map(ko => ko.function(mergedResults))
+    );
+    return {
+      intermediate: mergedResults,
+      final: finalResults
+    };
+  } catch (err) {
+    console.error(err);
+    return { error: 'Execution failed' };
+  }
+}
+
+
+module.exports = { run };
