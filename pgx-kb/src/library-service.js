d33d2d34454378936a21bfed56adb6ed9c0c2de9
Farid Seifi <farid.seifi@gmail.com>
2025-06-16 16:12:12 -0400

    adding initialize method to library service

--- pgx-kb/src/library-service.js
+++ pgx-kb/src/library-service.js
@@ -8,7 +8,7 @@ const path = require('path');
 let knowledgeSet1 = [];
 let knowledgeSet2 = [];
 
-async function run(input) {
+async function initialize(){
   const mainMeta = await loadMetadata(path.join(path.join(__dirname, '..'), 'metadata.json'));
   const lnpwledgeSets = mainMeta['https://kgrid.org/koio#hasKnowledge']
   knowledgeSet1 = await loadKnowledgeSet(lnpwledgeSets[0]);
@@ -17,6 +17,9 @@ async function run(input) {
   knowledgeSet1 = await loadKnowledgeFunctions(knowledgeSet1, 'phenotype');
   console.log(knowledgeSet1)
   knowledgeSet2 = await loadKnowledgeFunctions(knowledgeSet2, 'dosingrecommendation');
+}
+
+async function run(input) {
 
 try {
     const intermediateResults = await Promise.all(
@@ -39,4 +42,4 @@ try {
 }
 
 
-module.exports = { run };
+module.exports = { run, initialize };
